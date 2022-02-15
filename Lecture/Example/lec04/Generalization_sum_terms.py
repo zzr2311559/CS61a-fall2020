fab81fd13143e61161da7deddc34bff92776fd8d
zzr2311559 <58283596+zzr2311559@users.noreply.github.com>
2022-02-15 16:22:12 +0800

    Update Generalization_sum_terms.py

--- Lecture/Example/lec04/Generalization_sum_terms.py
+++ Lecture/Example/lec04/Generalization_sum_terms.py
@@ -36,7 +36,7 @@ def cube(k):
 def summation(n, term):
   """Sum the first N terms of a sequence.
   
-  >>>summation(5, cube)
+  >>> summation(5, cube)
   225
   """
   total = 0
@@ -45,4 +45,19 @@ def summation(n, term):
     total += term(k)
     k += 1
   return total
-    
+
+def sum_naturals(n):
+  """Sum the first N natural numbers
+  
+  >>> sum_naturals(5)
+  15
+  """
+  return summation(n, identity)
+  
+def sum_cubes(n):
+  """Sum the first N cubes of natural numbers
+  
+  >>> sum_cubes(5)
+  225
+  """
+  return summation(n, cube)
