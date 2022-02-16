924c59155f55c69fbfe4346d043ae589cb11c2a8
zzr2311559 <58283596+zzr2311559@users.noreply.github.com>
2022-02-16 14:58:10 +0800

    Create Composition.py

---
+++ Lecture/Q&A/lec04/Composition.py
@@ -0,0 +1,22 @@
+def compose1(f, g):
+  return lambda x: f(g(x))
+
+f = compose1(lambda x: x * x, lambda y: y + 1)
+result1 = f(12)
+
+"""------------------------------------------------------------
+   The code above is almost the same as below.
+"""
+def compose2(f, g):
+  def composition(x):
+    return f(g(x))
+  return composition
+
+def f1(x):
+  return x * x
+
+def f2(y):
+  return y + 1
+
+function = compose2(f1, f2)
+result2 = f(12)
