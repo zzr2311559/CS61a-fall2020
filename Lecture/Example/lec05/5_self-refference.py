729313d86b50a9e4d22e5f1d67bf2691a16ce024
zzr2311559 <58283596+zzr2311559@users.noreply.github.com>
2022-02-23 21:44:57 +0800

    Update 5_self-refference.py

--- Lecture/Example/lec05/5_self-refference.py
+++ Lecture/Example/lec05/5_self-refference.py
@@ -10,7 +10,7 @@ print_all(1)(3)(5)
 def print_sums(x):
   print(x)
   def next_sum(y):
-    return print_sums(x + y)
-  return print next_sum
+    return print_sums(x + y)   # Notice that we return a function call here 
+  return print next_sum        # Notice that we return a function here
 
 print_sums(1)(3)(5)
